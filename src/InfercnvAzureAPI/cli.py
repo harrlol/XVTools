97e819d567b0d67de6115c33506d5f4b3b8d38da
Harry Li <lidatou0708@gmail.com>
2025-09-02 15:33:38 +0000

    flag debugs

--- src/InfercnvAzureAPI/cli.py
+++ src/InfercnvAzureAPI/cli.py
@@ -129,7 +129,7 @@ if __name__ == "__main__":
     parser.add_argument("--no-denoise", dest="denoise", action="store_false")
     parser.add_argument("--HMM", action="store_true", default=True)
     parser.add_argument("--no-HMM", dest="HMM", action="store_false")
-    parser.add_argument("--ref_group_names", nargs="+", default=None, help="Reference group names for infercnv (default: None)")
+    parser.add_argument("--ref_group_names", nargs="*", default=None, help="Reference group names for infercnv (default: None)")
     parser.add_argument("--malig_name", type=str, default="malignant", help="Malignant cell type name (default: malignant)")
     args = parser.parse_args()
 
